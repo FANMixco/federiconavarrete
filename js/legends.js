358b11fc129c24256a781828d7649a69c3dd5403
Federico Navarrete <darklord.navarrete@gmail.com>
2019-10-31 08:19:09 +0100

    Added touch sensititivy
    - Now legends can be swiped by touch.
    - Standardized libraries.

--- js/legends.js
+++ js/legends.js
@@ -21,4 +21,19 @@ $(document).ready(function() {
         }
       }
     });
+	$(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+		$(this).one("touchmove", function(event){
+			var xMove = event.originalEvent.touches[0].pageX;
+			if( Math.floor(xClick - xMove) > 5 ){
+				$(this).carousel('next');
+			}
+			else if( Math.floor(xClick - xMove) < -5 ){
+				$(this).carousel('prev');
+			}
+		});
+		$(".carousel").on("touchend", function(){
+				$(this).off("touchmove");
+		});
+	});
 });
