50fb1cd03e070c7766e5ae495b1c80d327339c9f
Federico Navarrete <fanmixco@gmail.com>
2020-05-31 12:41:42 +0200

    Re-ordered awards

--- js/data/translations/en-us/awardsList.js
+++ js/data/translations/en-us/awardsList.js
@@ -4,25 +4,25 @@ const awardsList = {
 			{
 				"title": "President's Distinguished Area by Toastmasters International 2019"
 			},
-			{ 
-				"title": "1st place in the Faculty ranking of the WEEIA and IFE 2016"
-			}
-		],
-		[
-			{
-				"title": "Best scientific paper in the iNOTICE Conference 2016"
-			},
-			{
-				"title": "Top 5 in the Microsoft Imagine Cup 2016 (Poland), World Citizenship Category"
-			}
-		],
-		[
 			{
 				"title": `1st place in the Cisco Switch Up Challenge 2016, "The Internet of Everything"`
+			}
+		],
+		[
+			{
+				"title": "Top 5 in the Microsoft Imagine Cup 2016 (Poland) in World Citizenship"
 			},
 			{
-				"title": "Top 5 of The Most Inspiring projects of SpaceApps Challenge of NASA 2014",
+				"title": "Top 5 of The Most Inspiring Projects of NASA SpaceApps Challenge 2014",
 				"link": `https://2014.spaceappschallenge.org/awards/#globalawards`
+			}
+		],
+		[
+			{ 
+				"title": "1st place in the Faculty ranking of the WEEIA and IFE 2016"
+			},
+			{
+				"title": "Best scientific paper in the iNOTICE Conference 2016"
 			}
 		]
 	],
