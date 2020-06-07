8eb47df2d3b9013f14a5bf05a6daaddb3bdc65cd
Federico Navarrete <fanmixco@gmail.com>
2020-06-07 18:42:27 +0200

    Reduced size
    - Changed all images to JPG.
    - Reduced size considerably for loading.
    - Deferred Google Analytics.
    - Rename references file.

--- js/data/translations/en-us/basicInfo.js
+++ js/data/translations/en-us/basicInfo.js
@@ -3,11 +3,11 @@ const basicInfo = {
 	"headline": `IT Consultant 💡 and Orator 🇬🇧/ 🇪🇸`,
 	"headlineIntro": `It's time to bring innovation 💡! Are you ready?`,
 	"aboutDesc": [
-		`Welcome! I'm an IT Consultant and Orator 🇬🇧/ 🇪🇸 from <a id="aElSalvador" href="#elSalvador" class="text-material-link" data-toggle="modal" data-target="#elSalvador">El Salvador</a>, who can help you bring innovation and change to your world!<br /><br />I've been in the IT industry for over 10 years, where I've designed <a href="https://behance.net/fanmixco" class="text-material-link" target="_blank">innovative apps</a>, led fantastic teams, automated multiple services, and developed important <a href="https://www.youtube.com/c/FedericoNavarrete" class="text-material-link" target="_blank">soft skills</a>.`,
+		`Welcome! I'm an IT Consultant and Orator 🇬🇧/ 🇪🇸 from <a id="aElSalvador" href="#elSalvador" class="text-material-link" data-toggle="modal" data-target="#elSalvador">El Salvador</a>, who can help you bring innovation and change to your world!<br /><br />I've been in the IT industry for over 10 years, where I've designed <a href="https://bit.ly/3dIhfQW" class="text-material-link" target="_blank">innovative apps</a>, led fantastic teams, automated multiple services, and developed important <a href="https://bit.ly/3cA3SkE" class="text-material-link" target="_blank">soft skills</a>.`,
 		`Furthermore, I'm on the constant quest for <span class="font-weight-bold">the next big thing</span>! Plus, challenges define me. I love innovation contests like Microsoft Imagine Cup and create a better world.<br /><br /><span class="text-uppercase font-weight-bold text-center d-block"><i>What about you? Are you ready for your next challenge? <a id="linkContactMeAbout" class="text-warning" style="cursor: pointer;">We can work together on it.</a></i></span>`
 	],
 	"favApp": {
-		"link": `https://play.google.com/store/apps/details?id=tk.supernovaic.themayanroute`,
+		"link": `https://bit.ly/2UkxmMZ`,
 		"title": "The Mayan Route!",
 		"isVisible": true
 	},
