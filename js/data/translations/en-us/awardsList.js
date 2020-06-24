061214ca9d47c6030b8fe865bd870bcad7eab470
Federico Navarrete <darklord.navarrete@gmail.com>
2020-06-24 09:30:56 +0200

    Changed awards

--- js/data/translations/en-us/awardsList.js
+++ js/data/translations/en-us/awardsList.js
@@ -2,27 +2,28 @@ const awardsList = {
 	"awards": [
 		[
 			{
-				"title": "President's Distinguished Area by Toastmasters International 2019"
-			},
+				"title": "Distinguished Toastmaster (DTM) by Toastmasters International in 2020",
+				"link": "https://www.toastmasters.org/education/distinguished-toastmaster"
+			},			
 			{
-				"title": `1st place in the Cisco Switch Up Challenge 2016, "The Internet of Everything"`
+				"title": "President's Distinguished Area by Toastmasters International 2019"
 			}
 		],
 		[
 			{
-				"title": "Top 5 in the Microsoft Imagine Cup 2016 (Poland) in World Citizenship"
+				"title": `1st place in the Cisco Switch Up Challenge 2016, "The Internet of Everything"`
 			},
+			{
+				"title": "Top 5 in the Microsoft Imagine Cup 2016 (Poland) in World Citizenship"
+			}
+		],
+		[
 			{
 				"title": "Top 5 of The Most Inspiring Projects of NASA SpaceApps Challenge 2014",
 				"link": `https://2014.spaceappschallenge.org/awards/#globalawards`
-			}
-		],
-		[
+			},
 			{ 
 				"title": "1st place in the Faculty ranking of the WEEIA and IFE 2016"
-			},
-			{
-				"title": "Best scientific paper in the iNOTICE Conference 2016"
 			}
 		]
 	],
