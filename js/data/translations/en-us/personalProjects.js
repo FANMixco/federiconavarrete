31f3c3efb4f6445b21987f444611a238cb3d696e
Federico Navarrete <fanmixco@gmail.com>
2021-08-03 23:53:45 +0200

    New apps
    Updated apps

--- js/data/translations/en-us/personalProjects.js
+++ js/data/translations/en-us/personalProjects.js
@@ -1,21 +1,21 @@
 const personalProjects = [
 	{
-		"youTubeID": `0S8ybMuA4vA`,
-		"link": `https://github.com/FANMixco/toastmasters-timer`,
+		"youTubeID": '2_iI-uGmjcc',
+		"link": 'https://play.google.com/store/apps/details?id=tk.supernovaic.themayanroute',
+		"title": "The Mayan Route",
+		"timeFrame": "2013 - now",
+		"isActive": true	
+	},
+	{
+		"youTubeID": '0S8ybMuA4vA',
+		"link": 'https://fanmixco.github.io/toastmasters-timer-material-design',
 		"title": "Toastmasters Timer",
 		"timeFrame": "2018 - now",
-		"isActive": true
+		"isActive": false
 	},
 	{
-		"youTubeID": `HEfgCXWM6xg`,
-		"link": `https://www.microsoft.com/en-gb/p/lets-stop-diabetes-premium/9nblggh4r824`,
-		"title": "Let's Stop Diabetes",
-		"timeFrame": "2015 - now"	,
-		"isActive": false	
-	},
-	{
-		"youTubeID": `x-4Z8Fo7HOQ`,
-		"link": `http://gravitynow.tk`,
+		"youTubeID": 'x-4Z8Fo7HOQ',
+		"link": 'https://fanmixco.github.io/gravitynow-angular',
 		"title": "Gravity Now!",
 		"timeFrame": "2014 - now",
 		"isActive": false	
