a9480b4561618b7e1906505738c97929a1708585
¨Federico <fanmixco@gmail.com>
2021-11-16 07:30:10 +0100

    Upgraded skills

--- js/data/translations/en-us/techSkillsLists.js
+++ js/data/translations/en-us/techSkillsLists.js
@@ -1,18 +1,18 @@
 //techskills front
 const techSkills = [
 	[
-		`Agile - Scrum`,
-		`CICD — Azure DevOps`,
-		`<a href="https://fanmixco.github.io/rpaBestPractices/" target="_blank" class="text-warning" rel="noreferrer">RPA</a> — UiPath &amp; Automation Anywhere`
+		'Agile - Scrum',
+		'CICD — Azure DevOps',
+		'<a href="https://fanmixco.github.io/rpaBestPractices/" target="_blank" class="text-warning" rel="noreferrer">RPA</a> — UiPath &amp; Automation Anywhere'
 	],
 	[
-		"Angular 8+",
-		`PWA`,
-		`Google Maps API`
+		'Angular 12+',
+		'PWA',
+		'C# & .NET Core',
 	],
 	[
-		`Cloud / Mobile / Web Dev`,
-		`C# & .NET Core`,
+		'Cloud Architecture (AWS)',
+		'Cloud / Mobile / Web Dev',
 		"Xamarin.Android"
 	]
 ];
@@ -20,21 +20,21 @@ const techSkills = [
 //techskills hidden
 const techSkillsOthers =[
 	[
-		`ITSM — ITIL v4`,
-		`ATDD`,
-		`RESTful Web Services`,
-		`Versioning — Git`,
+		'ITSM — ITIL v4',
+		'ATDD',
+		'RESTful Web Services',
+		'Versioning — Git',
 	],
 	[
-		`ASP MVC`,
-		`Entity Framework`,
-		`HTML5 / CSS3 / JS`,
-		`UI & UX`
+		'ASP MVC',
+		'Entity Framework',
+		'HTML5 / CSS3 / JS',
+		'UI & UX'
 	],
 	[
-		`Azure (PaaS & IaaS)`,
-		`Visual Studio & Code`,
-		`SQL Server & SQLite`,
-		`Google Analytics`
+		'Azure (PaaS & IaaS)',
+		'Visual Studio & Code',
+		'SQL Server & SQLite',
+		'Google Analytics &amp; Maps API'
 	]
 ];
