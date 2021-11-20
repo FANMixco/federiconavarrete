20fd544fc286f17d7d8bb711247184ae58a523ba
¨Federico <fanmixco@gmail.com>
2021-11-20 14:08:29 +0100

    Better text

--- js/data/translations/en-us/basicInfo.js
+++ js/data/translations/en-us/basicInfo.js
@@ -1,10 +1,10 @@
 const basicInfo = {
 	"name": "Federico Navarrete",
 	"headline": 'Cloud Architect 💡, <a id="aBooksModal" data-toggle="modal" data-target="#booksModal" href="#booksModal">Author</a> 📝 and Orator 🇬🇧/🇪🇸<br />Leading Digital Transformations',
-	"headlineIntro": "It's time to bring innovation 💡 TO YOUR LIFE! Are you ready?",
+	"headlineIntro": "It's time to bring innovation TO YOUR LIFE! Are you ready?",
 	"aboutDesc": [
-		`Welcome! I'm a Cloud Architect, Author and Orator 🇬🇧/🇪🇸 from <a id="aElSalvador" href="#elSalvador" class="text-material-link" data-toggle="modal" data-target="#elSalvador">El Salvador</a>, who can help you bring innovation and change to your world!<br /><br />I've been in the IT industry for over 10 years, where I've designed <a href="https://bit.ly/3dIhfQW" class="text-material-link" rel="noreferrer" target="_blank">innovative apps</a>, led fantastic teams, automated multiple services, and developed important <a href="https://bit.ly/3cA3SkE" rel="noreferrer" class="text-material-link" target="_blank">soft skills</a>.`,
-		`Furthermore, I'm on the constant quest for <span class="font-weight-bold">the next big thing</span>! Plus, challenges define me. I love innovation contests like Microsoft Imagine Cup and creating a better world.<br /><br /><span class="text-uppercase font-weight-bold text-center d-block"><i>Are you ready for your next challenge?<br><a id="linkContactMeAbout" class="text-warning" style="cursor: pointer; font-size:1.75rem">CONTACT ME!</a></i></span>`
+		`Welcome! I'm a Cloud Architect, Author and Orator 🇬🇧/🇪🇸 from <a id="aElSalvador" href="#elSalvador" class="text-material-link" data-toggle="modal" data-target="#elSalvador">El Salvador</a>. I can help you bring innovation and change to your world!<br /><br />I've been in the IT industry for over 10 years, where I've led fantastic teams, co-created <a href="https://bit.ly/3dIhfQW" class="text-material-link" rel="noreferrer" target="_blank">innovative apps</a>, automated multiple services, and developed important <a href="https://bit.ly/3cA3SkE" rel="noreferrer" class="text-material-link" target="_blank">soft skills</a>.`,
+		`I'm always on the quest for <span class="font-weight-bold">the next big thing</span>! I love innovation and taking part in hackathons where I can create a better world 🌍 with technology.<br /><br /><span class="text-uppercase font-weight-bold text-center d-block"><i>What about you? Are you ready for your next challenge?</i><br><a id="linkContactMeAbout" class="text-warning" style="cursor: pointer; font-size:1.75rem">CONTACT ME 💡</a></span>`
 	],
 	"favApp": {
 		"link": 'https://bit.ly/2UkxmMZ',
