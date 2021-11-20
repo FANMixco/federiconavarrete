84350a449f6885f758634d3b7486dfea108a7815
Federico Navarrete <fanmixco@gmail.com>
2021-11-20 09:15:47 +0100

    Minor changes

--- js/data/translations/en-us/awardsList.js
+++ js/data/translations/en-us/awardsList.js
@@ -2,25 +2,26 @@ const awardsList = {
 	"awards": [
 		[
 			{ 
-				"title": "1<sup>st</sup> Place Citython 2021, Eindhoven and Hamburg Editions. Sponsored by the EU.",
+				"title": "1<sup>st</sup> Place in Citythons 2021, Eindhoven and Hamburg Editions. Sponsored by the EU.",
 				"link": "https://www.citython.eu/2021eindhoven"
 			},
 			{
+				"title": "Samsung Top Developer Award 2021, Social.",
+				"link": "https://news.samsung.com/es/samsung-dev-spain-celebra-su-evento-anual-con-desarrolladores-locales"
+			}			
+		],
+		[
+			{
 				"title": "Distinguished Toastmaster (DTM) by Toastmasters International in 2020",
 				"link": "https://www.toastmasters.org/education/distinguished-toastmaster"
-			}
-		],
-		[
-			{
-				"title": "President's Distinguished Area by Toastmasters International 2019"
 			},
 			{
-				"title": `1<sup>st</sup> place Cisco Switch Up Challenge 2016, "The Internet of Everything"`
+				"title": "President's Distinguished Area by Toastmasters International 2019"
 			}
 		],
 		[
 			{
-				"title": "Top 5 in the Microsoft Imagine Cup 2016 (Poland) in World Citizenship"
+				"title": `1<sup>st</sup> place Cisco Switch Up Challenge 2016, "The Internet of Everything"`
 			},
 			{
 				"title": "Top 5 of The Most Inspiring Projects of NASA SpaceApps Challenge 2014",
--- js/data/translations/en-us/basicInfo.js
+++ js/data/translations/en-us/basicInfo.js
@@ -4,7 +4,7 @@ const basicInfo = {
 	"headlineIntro": "It's time to bring innovation 💡 TO YOUR LIFE! Are you ready?",
 	"aboutDesc": [
 		`Welcome! I'm a Cloud Architect, Author and Orator 🇬🇧/🇪🇸 from <a id="aElSalvador" href="#elSalvador" class="text-material-link" data-toggle="modal" data-target="#elSalvador">El Salvador</a>, who can help you bring innovation and change to your world!<br /><br />I've been in the IT industry for over 10 years, where I've designed <a href="https://bit.ly/3dIhfQW" class="text-material-link" rel="noreferrer" target="_blank">innovative apps</a>, led fantastic teams, automated multiple services, and developed important <a href="https://bit.ly/3cA3SkE" rel="noreferrer" class="text-material-link" target="_blank">soft skills</a>.`,
-		`Furthermore, I'm on the constant quest for <span class="font-weight-bold">the next big thing</span>! Plus, challenges define me. I love innovation contests like Microsoft Imagine Cup and create a better world.<br /><br /><span class="text-uppercase font-weight-bold text-center d-block"><i>What about you? Are you ready for your next challenge? <a id="linkContactMeAbout" class="text-warning" style="cursor: pointer;">We can work together on it.</a></i></span>`
+		`Furthermore, I'm on the constant quest for <span class="font-weight-bold">the next big thing</span>! Plus, challenges define me. I love innovation contests like Microsoft Imagine Cup and creating a better world.<br /><br /><span class="text-uppercase font-weight-bold text-center d-block"><i>Are you ready for your next challenge?<br><a id="linkContactMeAbout" class="text-warning" style="cursor: pointer; font-size:1.75rem">CONTACT ME!</a></i></span>`
 	],
 	"favApp": {
 		"link": 'https://bit.ly/2UkxmMZ',
