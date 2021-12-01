56d77cf6f93fa8be38c735c855eada28f2d2d123
Federico Navarrete <fanmixco@gmail.com>
2021-12-01 15:24:45 +0100

    Added an extra book

--- js/data/translations/es-sv/booksList.js
+++ js/data/translations/es-sv/booksList.js
@@ -1,57 +1,62 @@
 const booksList = [
 	{
-		"title": "",
+		"title": "Harry Potter y la Orden del Fénix",
 		"img": "../img/books/es/HP.jpg",
 		"link": "https://amzn.to/3pi6YCg"
 	},
 	{
-		"title": "",
+		"title": "Timeless Stories of El Salvador",
 		"img": "../img/books/mybook1.jpg",
 		"link": "https://amzn.to/3nnbn4e",
 		"isActive": true
 	},
 	{
-		"title": "",
+		"title": "Como ganar amigos e influir sobre las personas",
 		"img": "../img/books/es/WF.jpg",
 		"link": "https://amzn.to/2ZEIzhg"
 	},
 	{
-		"title": "",
+		"title": "Cómo suprimir las preocupaciones y disfrutar de la vida",
 		"img": "../img/books/es/worry.jpg",
 		"link": "https://amzn.to/3EfkLzQ"
 	},
 	{
-		"title": "",
+		"title": "Your Next Five Moves (inglés)",
 		"img": "../img/books/en/moves.jpg",
 		"link": "https://amzn.to/3lriwSB"
 	},
 	{
-		"title": "",
+		"title": "Personal Branding with LinkedIn: The Think Natalia Method (inglés)",
 		"img": "../img/books/en/linkedin.jpg",
 		"link": "https://amzn.to/2MOgslZ"
 	},
 	{
-		"title": "",
+		"title": "El monje que vendió su Ferrari",
 		"img": "../img/books/es/monk.jpg",
 		"link": "https://amzn.to/3paJhM2",
 	},
 	{
-		"title": "",
+		"title": "Targeted (inglés)",
+		"img": "../img/books/en/targeted.jpg",
+		"link": "https://amzn.to/31h16AM"
+	},
+	{
+		"title": "Mala sangre: Secretos y mentiras en una startup de Silicon Valley",
 		"img": "../img/books/es/blood.jpg",
 		"link": "https://amzn.to/3dn4FZb"
 	},
 	{
-		"title": "",
+		"title": "The 25 Laws for Doing The Impossible (inglés)",
 		"img": "../img/books/en/impossible.jpg",
 		"link": "https://bit.ly/2L90eon"
 	},
 	{
-		"title": "",
+		"title": "Software Architecture for Developers (inglés)",
 		"img": "../img/books/en/arch.jpg",
 		"link": "https://bit.ly/3I29FjM"
 	},
 	{
-		"title": "",
+		"title": "User Story Mapping: Discover the Whole Story, Build the Right Product",
 		"img": "../img/books/en/sm.jpg",
 		"link": "https://bit.ly/377v3SN"
 	}
