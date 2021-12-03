acef85475bfaa1cf095138533fa84ea532305ac8
Federico Navarrete <fanmixco@gmail.com>
2021-12-03 10:58:24 +0100

    Added new book
    Optimized images

--- js/data/translations/es-sv/booksList.js
+++ js/data/translations/es-sv/booksList.js
@@ -2,62 +2,80 @@ const booksList = [
 	{
 		"title": "Harry Potter y la Orden del Fénix",
 		"img": "../img/books/es/HP.jpg",
+		"author": "J.K. Rowling",
 		"link": "https://amzn.to/3pi6YCg"
 	},
 	{
 		"title": "Timeless Stories of El Salvador",
 		"img": "../img/books/mybook1.jpg",
+		"author": "Federico Navarrete",
 		"link": "https://amzn.to/3nnbn4e",
 		"isActive": true
 	},
 	{
 		"title": "Como ganar amigos e influir sobre las personas",
 		"img": "../img/books/es/WF.jpg",
+		"author": "Dale Carnegie",
 		"link": "https://amzn.to/2ZEIzhg"
 	},
 	{
 		"title": "Cómo suprimir las preocupaciones y disfrutar de la vida",
 		"img": "../img/books/es/worry.jpg",
+		"author": "Dale Carnegie",
 		"link": "https://amzn.to/3EfkLzQ"
 	},
 	{
 		"title": "Your Next Five Moves (inglés)",
 		"img": "../img/books/en/moves.jpg",
+		"author": "Patrick Bet-David",
 		"link": "https://amzn.to/3lriwSB"
 	},
 	{
 		"title": "Personal Branding with LinkedIn: The Think Natalia Method (inglés)",
 		"img": "../img/books/en/linkedin.jpg",
+		"author": "Natalia Wiechowski",
 		"link": "https://amzn.to/2MOgslZ"
 	},
 	{
-		"title": "El monje que vendió su Ferrari",
-		"img": "../img/books/es/monk.jpg",
-		"link": "https://amzn.to/3paJhM2",
+		"title": "El coach de Sillicon Valley / Trillion Dollar Coach",
+		"img": "../img/books/es/coach.jpg",
+		"author": "Alan Eagle, Eric Schmidt, y Jonathan Rosenberg",
+		"link": "https://amzn.to/3ElMq1Y",
 	},
 	{
-		"title": "Targeted (inglés)",
+		"title": "Targeted: The Cambridge Analytica Whistleblower's Inside Story of How Big Data, Trump, and Facebook Broke Democracy and How It Can Happen Again (inglés)",
 		"img": "../img/books/en/targeted.jpg",
+		"author": "Brittany Kaiser",
 		"link": "https://amzn.to/31h16AM"
 	},
 	{
 		"title": "Mala sangre: Secretos y mentiras en una startup de Silicon Valley",
 		"img": "../img/books/es/blood.jpg",
+		"author": "John Carreyrou",
 		"link": "https://amzn.to/3dn4FZb"
 	},
 	{
 		"title": "The 25 Laws for Doing The Impossible (inglés)",
 		"img": "../img/books/en/impossible.jpg",
+		"author": "Patrick Bet-David",
 		"link": "https://bit.ly/2L90eon"
 	},
 	{
 		"title": "Software Architecture for Developers (inglés)",
 		"img": "../img/books/en/arch.jpg",
+		"author": "Simon Brown",
 		"link": "https://bit.ly/3I29FjM"
 	},
 	{
 		"title": "User Story Mapping: Discover the Whole Story, Build the Right Product",
 		"img": "../img/books/en/sm.jpg",
+		"author": "Jeff Patton",
 		"link": "https://bit.ly/377v3SN"
+	},
+	{
+		"title": "El monje que vendió su Ferrari",
+		"img": "../img/books/es/monk.jpg",
+		"author": "Robin Sharma",
+		"link": "https://amzn.to/3paJhM2",
 	}
 ];
