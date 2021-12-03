34ac3e1ab4e4faf556f1e94a6c3756733670d5ec
Federico Navarrete <fanmixco@gmail.com>
2021-12-03 07:59:04 +0100

    Fixed order

--- js/data/translations/es-sv/organizedEvents.js
+++ js/data/translations/es-sv/organizedEvents.js
@@ -2,7 +2,7 @@ const organizedEvents = {
 	"events": [
 		{
 			"link": 'https://fanmixco.github.io/LodzartsConference',
-			"title": 'Łódźarts - Conferencia Toastmasters, División A (inglés), <a href="https://toastmastersd108.org" rel="noreferrer" target="_blank" class="text-material-link-dark">District 108</a> - 2019',
+			"title": 'Łódźarts - Conferencia de Toastmasters - 2019 (inglés), División A, <a href="https://toastmastersd108.org" rel="noreferrer" target="_blank" class="text-material-link-dark">District 108</a>',
 			"imgID": "imgLodzarts",
 			"imgBasicName": "lodzarts",
 			"imgFormat": "jpg"
