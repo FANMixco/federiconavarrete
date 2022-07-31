85e317a5d16ced3b9aabf2607d9541618c40878c
Federico Navarrete <fanmixco@gmail.com>
2022-07-31 11:29:52 +0200

    Moved external JS

--- js/external.js
+++ js/external.js
@@ -1,4 +1,19 @@
 function onReadyExternal() {
+    getScript('https://www.googletagmanager.com/gtag/js?id=G-4X4X4PDHN7')
+    .then(() => {
+        loadAnalytics();
+    })
+    .catch((e) => {
+        console.error(e);
+    });
+
+    getScript('https://code.jivosite.com/widget/1D5ncamsoj')
+    .catch((e) => {
+        console.error(e);
+    });
+}
+
+function loadAnalytics() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
