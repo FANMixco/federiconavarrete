78478f7910056f5051a1b271f015072f19a68665
enavfed <darklord.navarrete@gmail.com>
2023-01-18 13:10:07 +0100

    New changes
    - Removed old data.
    - Added multilingual support.

---
+++ timeless-stories/js/translations.js
@@ -0,0 +1,20 @@
+let lang = 'en';
+
+let nLang = (navigator.languages
+    ? navigator.languages[0]
+    : (navigator.language || navigator.userLanguage)).split('-')[0];
+
+let supportedLang = ['en', 'es'];
+
+lang = supportedLang.includes(nLang) ? nLang : lang;
+
+getScript(`js/i18n/lang-${lang}.js`)
+.then(() => {
+    document.querySelectorAll('[data-translation]').forEach(item => {
+        console.log(item);
+        item.innerHTML = translations[`${item.dataset.translation}`];
+    });
+})
+.catch((e) => {
+    console.error(e);
+});
