b5e1503c235a1f010708c05d04505759af2e5332
Federico Navarrete <fanmixco@gmail.com>
2023-05-16 23:45:53 +0200

    Optimized code

---
+++ linktree/js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var imgPopups = document.getElementsByClassName('imgPopup');
+    for (var i = 0; i < imgPopups.length; i++) {
+      imgPopups[i].style.display = 'block';
+    }
+});
