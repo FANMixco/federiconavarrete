c0628be01a7875b62b8072ff424649809af34637
Federico Navarrete <fanmixco@gmail.com>
2023-05-16 16:43:28 +0200

    Minor improments

--- linktree/js/analytics.js
+++ linktree/js/analytics.js
@@ -1,5 +1,1 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-WR9ZRLLN09');
+window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date),gtag("config","G-WR9ZRLLN09");
