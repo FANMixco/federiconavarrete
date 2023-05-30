8d2a2ea12cf967a66c0f04addc05f7b02158c59b
Federico Navarrete <fanmixco@gmail.com>
2023-05-30 21:02:10 +0200

    Added iOS icons

--- linktree/js/index.js
+++ linktree/js/index.js
@@ -4,3 +4,12 @@ window.addEventListener('load', function() {
       imgPopups[i].style.display = 'block';
     }
 });
+
+// Check if the user agent string indicates macOS or iOS
+let matches = navigator.userAgent.match(/Macintosh|MacIntel|iPad|iPhone|iPod/g);
+// Check if the array is not null or empty
+if (matches && matches.length > 0) {
+  let shareIcon = document.getElementById('share-icon');
+  shareIcon.classList.remove('icon-share-alt');
+  shareIcon.classList.add('icon-ios_share');
+}
