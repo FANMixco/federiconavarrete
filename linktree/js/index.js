34a91bade535eebc27c11045a3e1b9daf30d5178
Federico Navarrete <fanmixco@gmail.com>
2023-05-31 21:15:09 +0200

    ShareAPI fix
    Hid shared button for unsupported browsers

--- linktree/js/index.js
+++ linktree/js/index.js
@@ -5,11 +5,17 @@ window.addEventListener('load', function() {
     }
 });
 
-// Check if the user agent string indicates macOS or iOS
-let matches = navigator.userAgent.match(/Macintosh|MacIntel|iPad|iPhone|iPod/g);
-// Check if the array is not null or empty
-if (matches && matches.length > 0) {
-  let shareIcon = document.getElementById('share-icon');
-  shareIcon.classList.remove('icon-share-alt');
-  shareIcon.classList.add('icon-ios_share');
+let shareIcon = document.getElementById('share-icon');
+
+// Check if the Web Share API is supported by the browser
+if (navigator.share) { 
+  // Check if the user agent string indicates macOS or iOS
+  let matches = navigator.userAgent.match(/Macintosh|MacIntel|iPad|iPhone|iPod/g);
+  // Check if the array is not null or empty
+  if (matches && matches.length > 0) {
+    shareIcon.classList.remove('icon-share-alt');
+    shareIcon.classList.add('icon-ios_share');
+  }
+} else {
+  shareIcon.style.display = 'none';
 }
--- linktree/js/share.js
+++ linktree/js/share.js
@@ -4,21 +4,16 @@ const shareIcon = document.getElementById('shareIcon');
 // Add a click event listener to the share icon
 shareIcon.addEventListener('click', (e) => {
   e.preventDefault();
-  // Check if the Web Share API is supported by the browser
-  if (navigator.share) {
-    // Use the Web Share API to share the current page
-    navigator.share({
-      title: "Get to know Federico Navarrete!",
-      text: 'Here you can check Federico Navarrete’s official websites: https://bit.ly/fanmixco',
-      url: window.location.href
+  // Use the Web Share API to share the current page
+  navigator.share({
+    title: "Get to know Federico Navarrete!",
+    text: 'Here you can check Federico Navarrete’s official websites: https://bit.ly/fanmixco',
+    url: window.location.href
+  })
+    .then(() => {
+      console.log('Shared successfully!');
     })
-      .then(() => {
-        console.log('Shared successfully!');
-      })
-      .catch((error) => {
-        console.error('Error sharing:', error);
-      });
-  } else {
-    console.log('Web Share API not supported in this browser.');
-  }
+    .catch((error) => {
+      alert('Error sharing:', error);
+    });
 });
