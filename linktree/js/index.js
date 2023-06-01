131e1d1ded79bab015953b7a3281c0db6a5838f7
Federico Navarrete <fanmixco@gmail.com>
2023-06-01 05:18:49 +0200

    Potential CSS fix

--- linktree/js/index.js
+++ linktree/js/index.js
@@ -4,8 +4,3 @@ window.addEventListener('load', function() {
       imgPopups[i].style.display = 'block';
     }
 });
-
-// Get the viewport height and multiply it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
