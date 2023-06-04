6fd0d7b7ef587bba82ecee7a9498a347ac08b9e8
Federico Navarrete <fanmixco@gmail.com>
2023-06-04 15:27:03 +0200

    Fixed mobile performance

--- linktree/js/index.js
+++ linktree/js/index.js
@@ -9,3 +9,10 @@ window.addEventListener('load', function() {
 let vh = window.innerHeight * 0.01;
 // Set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+const ua = navigator.userAgent.toLowerCase().match(/android|iphone|ipod|kaios|tizen|harmonyos|bdos/g);
+  // Check if the array is not null or empty
+if (ua && ua.length > 0) {
+  const uTubeLink = document.getElementById('uTubeLink');
+  uTubeLink.href = uTubeLink.href.replace('www', 'm');
+}
