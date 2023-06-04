6a8e24acd7c4ecc6f37b5a8942af9e8d31314f70
Federico Navarrete <fanmixco@gmail.com>
2023-06-04 22:18:10 +0200

    Fixed services

--- js/data/translations/zh-zh/servicesList.js
+++ js/data/translations/zh-zh/servicesList.js
@@ -2,24 +2,24 @@ const servicesList = {
 	"services": [
 		[
 			{
-				"title": "IT咨询或应用程序开发",
+				"title": "IT咨询或应用程序开发（英语或西班牙语）",
                 "link": "https://supernovaic.com/#contact",
 				"icon": "laptop-code-solid"
 			},
 			{
-                "title": "敏捷项目管理",
-                "link": "https://docs.google.com/forms/d/e/1FAIpQLScFtB86gL0vndcS56HCmLYSseJ8LsnNKZ0B2NpoFm2D7zRT-g/viewform",
-				"icon": "clock-fast"
-            }
-        ],
-        [
-			{
-				"title": "主题报告",
+				"title": "主题报告（英语或西班牙语）",
                 "link": "https://docs.google.com/forms/d/e/1FAIpQLSd98BjmX0KCrie2gnArng8vnbFCWYQeGxixAUSMJguXKF0i2A/viewform",
 				"icon": "speaker"
-			},
+			}
+		],
+        [,
 			{
-                "title": "电梯推介和讲故事培训",
+                "title": "敏捷项目管理（英语或西班牙语）",
+                "link": "https://docs.google.com/forms/d/e/1FAIpQLScFtB86gL0vndcS56HCmLYSseJ8LsnNKZ0B2NpoFm2D7zRT-g/viewform",
+				"icon": "clock-fast"
+            },
+			{
+                "title": "电梯推介和讲故事培训（英语或西班牙语）",
                 "link": "https://docs.google.com/forms/u/1/d/e/1FAIpQLScJTL_QYjtI8f_BWZSqqxQKfnHrCzmgQ9bGp45EJqqUgdxiCg/viewform",
 				"icon": "chalkboard-teacher-solid"
 			}
