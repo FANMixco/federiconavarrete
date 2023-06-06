979a90e40f53066c6dc0e42d65c425b1a348c47f
enavfed <darklord.navarrete@gmail.com>
2023-06-06 10:51:52 +0200

    Fixed UI

--- js/personal.js
+++ js/personal.js
@@ -26,3 +26,23 @@ if (ua && ua.length > 0 && window.matchMedia('(max-width: 768px)').matches) {
   const imgProfile = document.getElementById('imgProfile');
   imgProfile.classList.add('mb-5', 'd-block', 'mx-auto');
 }
+
+const profileDiv = document.getElementById('profile-div');
+const mediaQuery = window.matchMedia('(min-width: 769px)');
+
+function handleTabletChange(e) {
+  // Check if the media query is true
+  if (!e.matches) {
+    profileDiv.classList.remove('col-sm-auto');
+    profileDiv.classList.add('col-sm', 'pt-4', 'pb-4');
+  } else {
+    profileDiv.classList.remove('col-sm', 'pt-4', 'pb-4');
+    profileDiv.classList.add('col-sm-auto');
+  }
+}
+
+// Register event listener
+mediaQuery.addEventListener('change', handleTabletChange);
+
+// Initial check
+handleTabletChange(mediaQuery);
