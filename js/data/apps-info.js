f562ef3854e56ee9fd8d59cc2afc7d195b196e79
Federico Navarrete <fanmixco@gmail.com>
2023-06-18 09:00:38 +0200

    Fixed techs

--- js/data/apps-info.js
+++ js/data/apps-info.js
@@ -1412,7 +1412,7 @@ let apps =
                link: "",
                preview: "https://bit.ly/3x6P9b4",
                technologies: [
-                  "fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "mdi mdi-jquery", "fab fa-bootstrap", "fab fa-git", "mdi mdi-github", "mdi mdi-google-analytics"
+                  "fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", [{type: "text", id: "id_pwa", text: "PWA", tooltip: "Progressive Web App"}], "fab fa-git", "mdi mdi-github", "mdi mdi-google-analytics"
                ],
                order: 7
             }
