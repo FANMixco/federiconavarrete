974da21638114caee6aa003d3b360bc6983c1549
enavfed <darklord.navarrete@gmail.com>
2023-06-22 14:21:49 +0200

    Added multi-lingual support

--- linktree/js/loadData.js
+++ linktree/js/loadData.js
@@ -1,72 +1,67 @@
-fetch('js/data/basic.json')
-    .then(response => response.json())
-    .then(data => {
-        document.querySelectorAll('[data-translation]').forEach(item => {
-            item.innerHTML = data[`${item.dataset.translation}`];
+let language = window.navigator.userLanguage || window.navigator.language;
+let lang = "en";
+let currentLoc = '';
+
+const langLoc = "js/data/translations/";
+
+lang = (language.includes('es')) ? "es" : lang;
+
+async function fetchData(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+}
+
+function createLink(item, className) {
+    const link = document.createElement('a');
+    link.classList.add(className);
+    link.href = item.link;
+    link.setAttribute('aria-label', item['aria-label']);
+    link.setAttribute('title', item.title);
+
+    if (item.target) {
+        link.target = item.target;
+    }
+
+    const icon = document.createElement('i');
+    icon.classList.add(item.icon);
+
+    link.appendChild(icon);
+    return link;
+}
+
+function createTextElement(text) {
+    return document.createTextNode(text);
+}
+
+fetchData(`js/data/${lang}/basic.json`)
+.then(data => {
+    document.querySelectorAll('[data-translation]').forEach(item => {
+    item.innerHTML = data[item.dataset.translation];
     });
-})
-.catch(error => {
-    console.error('Error loading JSON:', error);
 });
 
-fetch('js/data/sublinks.json')
-    .then(response => response.json())
-    .then(data => {
-      // Get the container element
-      const linksContainer = document.getElementById('sub-links');
-      
-      // Iterate over the JSON data and create the links
-      data.forEach(item => {
-        const link = document.createElement('a');
-        link.classList.add('sub-link');
-        link.href = item.link;
-        link.setAttribute('aria-label', item['aria-label']);
-        link.setAttribute('title', item.title);
-        
-        if (item.target) {
-          link.target = item.target;
-        }
-        
-        const icon = document.createElement('i');
-        icon.classList.add(item.icon);
-        
-        link.appendChild(icon);
-        linksContainer.appendChild(link);
+fetchData(`js/data/${lang}/sublinks.json`)
+.then(data => {
+    const linksContainer = document.getElementById('sub-links');
+
+    data.forEach(item => {
+    const link = createLink(item, 'sub-link');
+    linksContainer.appendChild(link);
     });
-})
-.catch(error => {
-    console.error('Error loading JSON:', error);
 });
 
-fetch('js/data/links.json')
-    .then(response => response.json())
-    .then(data => {
-        // Get the container element
-        const linksContainer = document.getElementById('links');
-        
-        // Iterate over the JSON data and create the links
-        data.forEach(item => {
-        const link = document.createElement('a');
-        link.classList.add('link');
-        link.href = item.link;
-        link.setAttribute('aria-label', item['aria-label']);
-        link.setAttribute('title', item.title);
-        
-        if (item.target) {
-            link.target = item.target;
-        }
-        
-        const icon = document.createElement('i');
-        icon.classList.add(item.icon);
-        icon.innerHTML = "&nbsp;";
-        
-        const text = document.createTextNode(item.text);
-        
-        link.appendChild(icon);
-        link.appendChild(text);
-        linksContainer.appendChild(link);
+fetchData(`js/data/${lang}/links.json`)
+.then(data => {
+    const linksContainer = document.getElementById('links');
+
+    data.forEach(item => {
+    const link = createLink(item, 'link');
+    const text = createTextElement(item.text);
+    link.appendChild(text);
+    linksContainer.appendChild(link);
     });
-})
-.catch(error => {
-    console.error('Error loading JSON:', error);
 });
