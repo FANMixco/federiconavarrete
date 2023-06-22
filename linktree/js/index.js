aedb0c53c2bfbf42f2beb4214045b54290f0f71c
Federico Navarrete <fanmixco@gmail.com>
2023-06-22 16:25:26 +0200

    Loaded logic

--- linktree/js/index.js
+++ linktree/js/index.js
@@ -15,15 +15,13 @@ let vh = window.innerHeight * 0.01;
 // Set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-const ua = navigator.userAgent.toLowerCase().match(/android|iphone|ipod|kaios|tizen|harmonyos|bdos/g);
-  // Check if the array is not null or empty
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
+  const ua = navigator.userAgent.toLowerCase().match(/android|iphone|ipod|kaios|tizen|harmonyos|bdos/g);
   if (ua && ua.length > 0) {
     const uTubeLink = document.getElementById('uTubeLink');
     uTubeLink.href = uTubeLink.href.replace('www', 'm');
   }
-});
-
+};
 
 const popup = document.getElementById("popup");
 popup.addEventListener("click", goBack);
