9c0f346ed4e68662ba18be6141f2d0b6f1d68f41
enavfed <darklord.navarrete@gmail.com>
2023-06-26 11:16:13 +0200

    Added new article

--- js/data/translations/en-us/articlesList.js
+++ js/data/translations/en-us/articlesList.js
@@ -1,17 +1,17 @@
 const articlesList = {
 	"articles": [
 		{
+			"link": `https://shorturl.at/pBCJ5`,
+			"title": `Can ChatGPT be your Copilot?`,
+			"imgID": "chatGPT",
+			"imgBasicName": "chatGPT",
+			"imgFormat": "jpg"
+		},
+		{
 			"link": `https://bit.ly/377HDCm`,
 			"title": `Is there any RPA platform for Android/iOS? 🤔`,
 			"imgID": "imgArt1",
 			"imgBasicName": "rpas1",
-			"imgFormat": "jpg"
-		},
-		{
-			"link": `https://bit.ly/3C0wUI5`,
-			"title": `The Four Faces of Automation in the Industry 4.0`,
-			"imgID": "imgArt2",
-			"imgBasicName": "faces2",
 			"imgFormat": "jpg"
 		}
 	],
--- js/data/translations/es-sv/articlesList.js
+++ js/data/translations/es-sv/articlesList.js
@@ -1,17 +1,17 @@
 const articlesList = {
 	"articles": [
 		{
+			"link": `https://shorturl.at/pBCJ5`,
+			"title": `¿Puede ChatGPT ser tu copiloto? (inglés)`,
+			"imgID": "chatGPT",
+			"imgBasicName": "chatGPT",
+			"imgFormat": "jpg"
+		},
+		{
 			"link": `https://bit.ly/377HDCm`,
 			"title": `¿Existe alguna plataforma RPA para Android/iOS? 🤔 (inglés)`,
 			"imgID": "imgArt1",
 			"imgBasicName": "rpas1",
-			"imgFormat": "jpg"
-		},
-		{
-			"link": `https://bit.ly/3C0wUI5`,
-			"title": `Las cuatro caras de la automatización en la industria 4.0 (inglés)`,
-			"imgID": "imgArt2",
-			"imgBasicName": "faces2",
 			"imgFormat": "jpg"
 		}
 	],
