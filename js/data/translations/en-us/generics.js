ece3be6f5e622ceea8abd2bd118dfb2c32287854
Federico Navarrete <fanmixco@gmail.com>
2024-01-01 19:08:43 +0100

    Minor change

--- js/data/translations/en-us/generics.js
+++ js/data/translations/en-us/generics.js
@@ -52,5 +52,5 @@ const genericTranslations = {
     "knowMoreBody": "Here you can check Federico Navarrete’s official websites: https://federiconavarrete.com",
     "share": "Share",
     "extras": "Extras",
-    "quote": "Like the Phoenix, Rise from YOUR ashes stronger each time."
+    "quote": "Like the Phoenix, Rise from YOUR ashes stronger every time."
 };
