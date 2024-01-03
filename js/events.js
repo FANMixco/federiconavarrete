44a9ea52e6d6e976065f58eab3f2fea9e2301c48
enavfed <darklord.navarrete@gmail.com>
2024-01-03 15:27:24 +0100

    Fixed mobile bugs

--- js/events.js
+++ js/events.js
@@ -1,7 +1,5 @@
 
 $('#eventsCarousel').on('slide.bs.carousel', function (e) {
-
-  
   var $e = $(e.relatedTarget);
   var idx = $e.index();
   var itemsPerSlide = 4;
@@ -19,6 +17,22 @@ $('#eventsCarousel').on('slide.bs.carousel', function (e) {
           }
       }
   }
+});
+
+$(".carousel").on("touchstart", function(event){
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > 5 ){
+            $(".carousel").carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -5 ){
+            $(".carousel").carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
 });
 
 $('#eventsCarousel').carousel({ 
