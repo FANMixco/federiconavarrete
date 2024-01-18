4bce0791d82d722abe0a6af832b53f1351b74497
enavfed <darklord.navarrete@gmail.com>
2024-01-18 10:58:12 +0100

    Fixed bug

--- js/libs/translations.js
+++ js/libs/translations.js
@@ -768,4 +768,4 @@ function changeModalType() {
 
 changeModalType();
 
-window.addEventListener("change", changeModalType);
+window.matchMedia("(orientation: portrait)").addEventListener("change", changeModalType);
