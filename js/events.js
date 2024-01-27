e11f8827d01ed3c81e9d35a05ede81998b7c2b9b
Federico Navarrete <fanmixco@gmail.com>
2024-01-27 09:27:35 +0100

    Changed code for JS mainly

--- js/events.js
+++ js/events.js
@@ -1,37 +1,42 @@
+document.getElementById('eventsCarousel').addEventListener('slide.bs.carousel', function (e) {
+    var relatedTarget = e.relatedTarget;
+    var idx = Array.from(relatedTarget.parentNode.children).indexOf(relatedTarget);
+    var itemsPerSlide = 4;
+    var totalItems = document.querySelectorAll('.carousel-item').length;
 
-$('#eventsCarousel').on('slide.bs.carousel', function (e) {
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 4;
-  var totalItems = $('.carousel-item').length;
-  
-  if (idx >= totalItems-(itemsPerSlide-1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var i=0; i<it; i++) {
-          // append slides to end
-          if (e.direction=="left") {
-              $('.carousel-item').eq(i).appendTo('.carousel-inner');
-          }
-          else {
-              $('.carousel-item').eq(0).appendTo('.carousel-inner');
-          }
-      }
-  }
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction === "left") {
+                document.querySelector('.carousel-item').parentNode.appendChild(
+                    document.querySelector('.carousel-item')
+                );
+            } else {
+                document.querySelector('.carousel-inner').insertBefore(
+                    document.querySelector('.carousel-item'),
+                    document.querySelector('.carousel-inner').firstChild
+                );
+            }
+        }
+    }
 });
 
-$(".carousel").on("touchstart", function(event){
-    var xClick = event.originalEvent.touches[0].pageX;
-    $(this).one("touchmove", function(event){
-        var xMove = event.originalEvent.touches[0].pageX;
-        if( Math.floor(xClick - xMove) > 5 ){
-            $(".carousel").carousel('next');
-        }
-        else if( Math.floor(xClick - xMove) < -5 ){
-            $(".carousel").carousel('prev');
+document.querySelector(".carousel").addEventListener("touchstart", function (event) {
+    var xClick = event.touches[0].pageX;
+
+    this.addEventListener("touchmove", function (event) {
+        var xMove = event.touches[0].pageX;
+
+        if (Math.floor(xClick - xMove) > 5) {
+            this.carousel('next');
+        } else if (Math.floor(xClick - xMove) < -5) {
+            this.carousel('prev');
         }
     });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
+
+    this.addEventListener("touchend", function () {
+        this.removeEventListener("touchmove");
     });
 });
 
