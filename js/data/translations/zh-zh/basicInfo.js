56e22633fcba7aea9d6dca062fb05d28bf075fe2
Federico Navarrete <fanmixco@gmail.com>
2024-02-01 13:48:11 +0100

    Minor changes

--- js/data/translations/zh-zh/basicInfo.js
+++ js/data/translations/zh-zh/basicInfo.js
@@ -3,7 +3,7 @@ const basicInfo = {
 	"headline": '云架构师 💡, <a id="aBooksModal" data-bs-toggle="modal" data-target="#booksModal" href="#booksModal">著者</a> 📝 和演说家 🇬🇧/🇪🇸<br />领先的数字化转型 🌐',
 	"headlineIntro": "是时候给你的生活带来创新了！你准备好了吗？",
 	"aboutDesc": [
-		`欢迎，我是Federico，云架构师、作家和演说家🇬🇧/🇪🇸. 我热衷于创造一个更美好的世界🌍 通过技术和创新，始终追求下一件大事.<br /><br />我住在四个国家，<a id="aElSalvador" href="#elSalvador" class="text-material-link" data-bs-toggle="modal" data-bs-target="#elSalvador" class="text-decoration-none">萨尔瓦多</a>，美国、波兰和西班牙，我在那里与30多个不同国籍的公民合作，使我能够建立<b><i>文化智能。</i></b>`,
+		`欢迎，我是Federico，云架构师、作家和演说家🇬🇧/🇪🇸. 我热衷于创造一个更美好的世界🌍 通过技术和创新，始终追求下一件大事.<br /><br />我住在四个国家，<a id="aElSalvador" href="#elSalvador" class="text-material-link" data-bs-toggle="modal" data-bs-target="#elSalvador" class="text-decoration-none">萨尔瓦多</a>，美国、波兰和西班牙，我在那里与30多个不同国籍的公民合作，使我能够建立<<span class="fw-bold fst-italic">>文化智能。</span>`,
 		`我独特的旅程让我能够带领出色的团队，共同创造 <a href="https://bit.ly/3dIhfQW" class="text-material-link" rel="noreferrer" target="_blank">创新应用程序</a>, 出版开创性的萨尔瓦多民俗书籍, 自动化多种服务，并开发出令人印象深刻的 <a id="youTubePreview" data-bs-toggle="modal" data-bs-target="#linkPreviews" class="text-material-link" target="_blank">公共演讲技巧</a>。<br /><br /><span class="text-uppercase fw-bold text-center d-block"><i>那你呢？你准备好迎接下一个挑战了吗？</i><br><a id="linkContactMeAbout" class="text-warning">联系我 💡</a></span>`
 	],
 	"favApp": {
