f4f43353ed13a115b57e51b8e10dfc36a3d77ca4
enavfed <darklord.navarrete@gmail.com>
2024-02-14 10:02:49 +0100

    Minor issue

--- js/external.js
+++ js/external.js
@@ -30,7 +30,7 @@ window.onload = function() {
     onReadyExternal();
 };
 
-if (!validLang.includes(language)) {
+if (!validLang.includes(language.split('-')[0])) {
     function googleTranslateElementInit() {
         new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'nl,de,fr,it,en,pt', autoDisplay: false, layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
     }
--- js/freelancer.js
+++ js/freelancer.js
@@ -84,15 +84,12 @@ function extraEvents(scroll) {
     else if (scroll > getHeight() * 0.7) {
         const gScriptExist = document.getElementById('g_translate');
         
-        if (!gScriptExist && validLang.includes(language)) {
+        if (!gScriptExist && validLang.includes(language.split('-')[0])) {
             const script = document.createElement('script');
             script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
             script.id = 'g_translate';
             document.body.appendChild(script);
-        }
-        else {
-            const gte = document.getElementById('google_translate_element');
-            gte.style.display = 'none';
+            document.getElementById('google_translate_element').classList.remove('d-none');
         }
     }
 }
