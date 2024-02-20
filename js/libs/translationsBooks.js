b3def01725646618880d45f2dce45d88ef7a3ff6
Federico Navarrete <fanmixco@gmail.com>
2024-02-20 13:09:53 +0100

    Merged code
    - Removed old libs.
    - Merged code.
    - Backed up old files in new folders.

--- js/libs/translationsBooks.js
+++ js/libs/translationsBooks.js
@@ -39,4 +39,28 @@ function loadBooks(booksList) {
     });
 
     document.getElementById('divBooks').appendChild(fragment);
+
+    $('#booksCarousel').on('slide.bs.carousel', function (e) {
+        /*
+            CC 2.0 License Iatek LLC 2018
+            Attribution required
+        */
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 7;
+        var totalItems = $('.carousel-item').length;
+        
+        if (idx >= totalItems-(itemsPerSlide-1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i=0; i<it; i++) {
+                // append slides to end
+                if (e.direction=="left") {
+                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                }
+                else {
+                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                }
+            }
+        }
+    });
 }
--- js/libs/translationsEvents.js
+++ js/libs/translationsEvents.js
@@ -17,6 +17,42 @@ window.addEventListener('DOMContentLoaded', async () => {
     } catch (e) {
         console.error(e);
     }
+
+    $('#eventsCarousel').on('slide.bs.carousel', function (e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 4;
+        var totalItems = $('.carousel-item').length;
+        
+        if (idx >= totalItems-(itemsPerSlide-1)) {
+            var it = itemsPerSlide - (totalItems - idx);
+            for (var i=0; i<it; i++) {
+                // append slides to end
+                if (e.direction=="left") {
+                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
+                }
+                else {
+                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+                }
+            }
+        }
+    });
+      
+    $(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+        $(this).one("touchmove", function(event){
+            var xMove = event.originalEvent.touches[0].pageX;
+            if( Math.floor(xClick - xMove) > 5 ){
+                $(".carousel").carousel('next');
+            }
+            else if( Math.floor(xClick - xMove) < -5 ){
+                $(".carousel").carousel('prev');
+            }
+        });
+        $(".carousel").on("touchend", function(){
+                $(this).off("touchmove");
+        });
+    });
 });
 
 function loadSeenOn(seenOnList) {
