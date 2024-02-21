2271b4e642de12f6805e0bc3a41611d1f454c240
Federico Navarrete <fanmixco@gmail.com>
2024-02-21 18:50:44 +0100

    Fixed translations issue

--- timeless-stories/js/translations.js
+++ timeless-stories/js/translations.js
@@ -1,5 +1,3 @@
-let lang = 'en';
-
 let nLang = (navigator.languages
     ? navigator.languages[0]
     : (navigator.language || navigator.userLanguage)).split('-')[0];
@@ -8,9 +6,9 @@ let supportedLang = ['en', 'es', 'zh'];
 
 let translations;
 
-lang = supportedLang.includes(nLang) ? nLang : lang;
+const lang = supportedLang.includes(nLang) ? nLang : 'en';
 
-fetchData(`js/i18n/lang-${lang}.json`)
+fetchData(`js/i18n/lang-${lang}.min.json`)
 .then((data) => {
     translations = data.translations;
     document.querySelectorAll('[data-translation]').forEach(item => {
