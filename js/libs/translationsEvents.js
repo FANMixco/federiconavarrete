ff732aaafb29beb680294f5649cd8a381e4fac67
enavfed <darklord.navarrete@gmail.com>
2024-02-21 08:35:44 +0100

    Removed old JS
    - Moved touch logic to CSS.

--- js/libs/translationsEvents.js
+++ js/libs/translationsEvents.js
@@ -37,22 +37,6 @@ window.addEventListener('DOMContentLoaded', async () => {
             }
         }
     });
-      
-    $(".carousel").on("touchstart", function(event){
-        let xClick = event.originalEvent.touches[0].pageX;
-        $(this).one("touchmove", function(event) {
-            let xMove = event.originalEvent.touches[0].pageX;
-            if (Math.floor(xClick - xMove) > 5) {
-                $(".carousel").carousel('next');
-            }
-            else if( Math.floor(xClick - xMove) < -5) {
-                $(".carousel").carousel('prev');
-            }
-        });
-        $(".carousel").on("touchend", function() {
-            $(this).off("touchmove");
-        });
-    });
 });
 
 function loadSeenOn(seenOnList) {
