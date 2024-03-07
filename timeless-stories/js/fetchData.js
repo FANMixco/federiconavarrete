c2a11c6f9384a4796b4a76ec1725d0eaf466e5e3
enavfed <darklord.navarrete@gmail.com>
2024-03-07 15:50:39 +0100

    Corrected SEO
    - Fixed potential SEO challenges

--- timeless-stories/js/fetchData.js
+++
@@ -1,8 +0,0 @@
-async function fetchData(url) {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch (error) {
-        console.error('Error loading JSON:', error);
-    }
-}
--- timeless-stories/js/gAnalytics.js
+++
@@ -1,1 +0,0 @@
-window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag("js",new Date),gtag("config","G-L3K2NVBX01");
