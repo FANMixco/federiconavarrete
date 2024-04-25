879eaf9f18b75b6c7818e3fe9e75b8c372b8b840
Federico Navarrete <fanmixco@gmail.com>
2024-04-25 19:52:19 +0200

    Fixed cache

--- pages/easter-egg/pwabuilder-sw.js
+++ pages/easter-egg/pwabuilder-sw.js
@@ -1,10 +1,13 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+//This is the service worker with the Advanced caching
 
 const CACHE = "pwabuilder-offline-page";
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const HTML_CACHE = "html";
+const JS_CACHE = "javascript";
+const STYLE_CACHE = "stylesheets";
+const IMAGE_CACHE = "images";
+const FONT_CACHE = "fonts";
 const offlineFallbackPage = "e404.html";
 
 self.addEventListener("message", (event) => {
@@ -15,40 +18,102 @@ self.addEventListener("message", (event) => {
 
 self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+  caches.open(CACHE)
+    .then((cache) => {
+    // Create a request for the offline page
+    const offlineRequest = new Request(offlineFallbackPage);
+    // Create a response for the offline page
+    const offlineResponse = new Response('<html><body><h1>Offline</h1></body></html>', {
+      headers: {'Content-Type': 'text/html'}
+    });
+    // Put them in the cache
+    return cache.put(offlineRequest, offlineResponse);
+    })
   );
 });
 
 if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
+    workbox.navigationPreload.enable();
 }
 
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith((async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+  
+          if (preloadResp) {
+            return preloadResp;
+          }
+  
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+  
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })());
+    }
+});
+
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+  ({event}) => event.request.destination === 'document',
+  new workbox.strategies.NetworkFirst({
+    cacheName: HTML_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 10,
+      }),
+    ],
   })
 );
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'script',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: JS_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
 
-        if (preloadResp) {
-          return preloadResp;
-        }
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'style',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: STYLE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'image',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: IMAGE_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
 
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
+workbox.routing.registerRoute(
+  ({event}) => event.request.destination === 'font',
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: FONT_CACHE,
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 15,
+      }),
+    ],
+  })
+);
